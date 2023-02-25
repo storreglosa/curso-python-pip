4258279a1874fa91cbb2264192b17d9d7db5af0a
Santiago td <santiagotorreglosadiaz@gmail.com>
2023-02-24 23:24:00 -0500

    Ajuste para guardar los gráficos de app

--- app/charts.py
+++ app/charts.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 def generate_bar_chart(labels, values):
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.show()
+  plt.save('bar.png')
+  plt.close()
 
 def generate_pie_chart(labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.show()
+  plt.save('pie.png')
+  plt.close()
 
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
